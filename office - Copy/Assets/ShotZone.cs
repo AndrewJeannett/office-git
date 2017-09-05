83ee2a34775af33a16ac3f5370f2ab5c9395992e
AndrewJeannett <andrewjeannett@gmail.com>
2017-09-04 23:11:53 -0500

    fixed shots

---
+++ office - Copy/Assets/ShotZone.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+
+public class ShotZone : MonoBehaviour
+{
+
+    public Shot targetShot;
+
+    void OnTriggerEnter(Collider c)
+    {
+
+        if (c.CompareTag("Player"))
+        {
+
+            targetShot.CutToShot();
+
+        }
+
+    }
+
+}
---
+++ office - Copy/Assets/TankControls.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+
+public class TankControls : MonoBehaviour
+{
+
+
+
+    public float moveSpeed = 3.0f;
+
+    public float rotationSpeed = 90.0f;
+
+
+
+    // Update is called once per frame
+
+    void Update()
+    {
+
+
+
+        float rotateCharacter = Input.GetAxis("Horizontal");
+
+        float moveCharacter = Input.GetAxis("Vertical");
+
+        Debug.Log("Rotation: " + rotateCharacter);
+
+
+
+        GetComponent<Rigidbody>().velocity = transform.forward * moveSpeed * moveCharacter;
+
+
+
+        transform.Rotate(Vector3.up * rotationSpeed * rotateCharacter * Time.deltaTime);
+
+    }
+
+}
