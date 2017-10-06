993c05e5a3b709ba645260ffef4255a1d0182b89
AndrewJeannett <andrewjeannett@gmail.com>
2017-10-06 14:42:44 -0500

    artic monkeys!

---
+++ office - Copy/Assets/infinitydetect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class infinitydetect : MonoBehaviour {
+    public GameObject pos;
+    private void OnTriggerEnter(Collider col)
+    {
+if (col.CompareTag ("Player")){
+            pos.transform.Translate(0, +1000, 0);
+       
+        }
+    }
+}
